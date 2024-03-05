de36ada3e974817ee08b5691547ebb786585f01e
jen2603 <jen2603@utulsa.edu>
2024-03-05 13:26:41 -0600

    first-test

---
+++ src/app/gameboard/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+
+import React, {useState} from "react";
+import ImageUploader from "./_components/ImageUploader.js"
+
+export default function GameBoard() {
+    return (
+        <div className="">
+            <ImageUploader/>
+        </div>
+    )
+}
--- src/components/auth/Authentication.js
+++ src/components/auth/Authentication.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const Authentication = () => {
   const [username, setUsername] = useState(''); // username state variable and setUsername function to update it
