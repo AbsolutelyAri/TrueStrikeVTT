929b7f15499b5d5f0e7c5a5a7691472af4d49b02
sherekhan9 <Sherekhan9@gmail.com>
2024-03-13 00:05:41 -0500

    Testing File Upload and Some small debuging

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import {useState} from 'react';
 function App() {
+  const [backgroundImage, setBackgroundImage] = useState('');
+
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setBackgroundImage(reader.result);
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
-  <p>hello worldS</p>
+    <div style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <input type="file" onChange={handleImageUpload} />
+    </div>
   );
 }
 
