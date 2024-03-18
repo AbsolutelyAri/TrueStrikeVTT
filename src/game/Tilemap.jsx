88fd48ebf714163ff2dc3f71a1c4e632d82459a9
jen2603 <jen2603@utulsa.edu>
2024-03-17 20:27:04 -0500

    Update Tilemap.jsx

--- src/game/Tilemap.jsx
+++ src/game/Tilemap.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useMemo } from "react";
 
 function Tilemap({onSelectColor, onSelectLoc}) {
     const canvasRef = useRef(null)
