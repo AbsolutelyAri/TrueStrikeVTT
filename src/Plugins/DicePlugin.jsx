2ce91c1165df1db3267f972b3c039f9d564f06e7
sherekhan9 <Sherekhan9@gmail.com>
2024-03-31 20:28:40 -0500

    Adding some small formating to Dice rolling

--- src/Plugins/DicePlugin.jsx
+++ src/Plugins/DicePlugin.jsx
@@ -1,24 +1,29 @@
 import React, { useState } from 'react';
 import { usePlugins } from './PluginProvider';
 import Draggable from 'react-draggable';
+import styles from './dicePlugin.module.css';
+
 function DicePlugin() {
     const { plugins } = usePlugins();
     const dicePlugin = plugins.find(plugin => plugin.name === "DicePlugin");
+
     const [rollResult, setRollResult] = useState(null);
+
     const diceRoll = () => {
-      const diceType = 6;
-      const roll = Math.floor(Math.random() * diceType) + 1;
-      setRollResult(`You rolled a ${roll}`);
+        const diceType = 6;
+        const roll = Math.floor(Math.random() * diceType) + 1;
+        setRollResult(`You rolled a ${roll} on a d${diceType}`);
     };
 
-
     return dicePlugin && dicePlugin.enabled ? (
-      <Draggable>
-        <div>
-                <button onClick={diceRoll}>Roll Dice</button>
-                {rollResult && <p>{rollResult}</p>}
-        </div>
-      </Draggable>
+        <Draggable>
+            <div className={styles.box}>
+                <div className={styles.buttonContainer}>
+                    <button className={styles.button} onClick={diceRoll}>Roll Dice</button>
+                    {rollResult && <p className={styles.canvasP}>{rollResult}</p>}
+                </div>
+            </div>
+        </Draggable>
     ) : null;
 }
 
