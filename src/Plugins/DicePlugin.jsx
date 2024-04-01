28e30aed8b9cc1b9b9427da415056b0875c4b813
Aaron205 <amr6717@utulsa.edu>
2024-04-01 11:44:01 -0500

    Revert "Adding some small formating to Dice rolling"
    This reverts commit 2ce91c1165df1db3267f972b3c039f9d564f06e7.

--- src/Plugins/DicePlugin.jsx
+++ src/Plugins/DicePlugin.jsx
@@ -1,29 +1,24 @@
 import React, { useState } from 'react';
 import { usePlugins } from './PluginProvider';
 import Draggable from 'react-draggable';
-import styles from './dicePlugin.module.css';
-
 function DicePlugin() {
     const { plugins } = usePlugins();
     const dicePlugin = plugins.find(plugin => plugin.name === "DicePlugin");
-
     const [rollResult, setRollResult] = useState(null);
-
     const diceRoll = () => {
-        const diceType = 6;
-        const roll = Math.floor(Math.random() * diceType) + 1;
-        setRollResult(`You rolled a ${roll} on a d${diceType}`);
+      const diceType = 6;
+      const roll = Math.floor(Math.random() * diceType) + 1;
+      setRollResult(`You rolled a ${roll}`);
     };
 
+
     return dicePlugin && dicePlugin.enabled ? (
-        <Draggable>
-            <div className={styles.box}>
-                <div className={styles.buttonContainer}>
-                    <button className={styles.button} onClick={diceRoll}>Roll Dice</button>
-                    {rollResult && <p className={styles.canvasP}>{rollResult}</p>}
-                </div>
-            </div>
-        </Draggable>
+      <Draggable>
+        <div>
+                <button onClick={diceRoll}>Roll Dice</button>
+                {rollResult && <p>{rollResult}</p>}
+        </div>
+      </Draggable>
     ) : null;
 }
 
